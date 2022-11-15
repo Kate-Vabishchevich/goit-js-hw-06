7534f660ebb914ed3c26132bba24ffbeecf451fb
Macbook <108225103+Kate-Vabishchevich@users.noreply.github.com>
2022-11-15 09:10:32 +0200

    Оновлено

--- js/task-08.js
+++ js/task-08.js
@@ -20,40 +20,16 @@
 //  використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
 
-// loginForm.addEventListener('submit', sendingForm)
+const loginForm = document.querySelector('form.login-form')
+loginForm.addEventListener('submit', sendingForm)
 
-// function sendingForm(event) {
-//     event.preventDefault();
-//     const { elements: { email, password } } = event.currentTarget;
-//     if (email.value === '' || password.value === '') {
-//         return alert('Please, fill in all the fields!');
-//     }
-//     console.log(`Email: ${email.value}, Password: ${password.value}`);
-//  event.currentTarget.reset();
-// }
-
-const form = document.querySelector("form.login-form");
-const input = document.querySelector("input");
-
-form.addEventListener("submit", onFormSubmit);
-
-function onFormSubmit(evt) {
-  evt.preventDefault();
-
-  const formElements = evt.currentTarget.elements;
-
-  const formData = new FormData(evt.currentTarget);
-
-  const email = formElements.email.value.trim().length;
-  const password = formElements.password.value.trim().length;
-  if (email === 0 || password === 0) {
-    return alert("Please, fill in all the fields!");
-  }
-
-    formData.forEach((value, name) => {
-    console.log("Name:", name);
-    console.log("Value:", value);
-  });
-
-  evt.currentTarget.reset();
+function sendingForm(event) {
+    event.preventDefault();
+    const { elements: { email, password } } = event.currentTarget;
+    if (email.value === '' || password.value === '') {
+        return alert('Please, fill in all the fields!');
+    }
+    // console.log(`Email: ${email.value}, Password: ${password.value}`);
+    console.log({ Email: email.value, Password: password.value});
+ event.currentTarget.reset();
 }
